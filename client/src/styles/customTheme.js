da2180db908660a433bd2f33b0d0858ba00139f8
udit2529 <uditmaheshwari17@gmail.com>
2023-05-06 23:00:43 +0530

    reddish

---
+++ client/src/styles/customTheme.js
@@ -0,0 +1,23 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const customTheme = (darkMode) =>
+  createMuiTheme({
+    palette: {
+      type: darkMode ? 'dark' : 'light',
+      primary: {
+        main: darkMode ? '#c0c0c0' : '#2A9D8F',
+      },
+      secondary: {
+        main: darkMode ? '#eeeeee' : '#264653',
+      },
+    },
+    overrides: {
+      MuiTypography: {
+        root: {
+          wordBreak: 'break-word',
+        },
+      },
+    },
+  });
+
+export default customTheme;
